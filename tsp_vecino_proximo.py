7f81385f3f79a3b13ce8d35414261f4db562a405
goldem64 <goldem64.master@aol.com>
2013-11-22 06:11:17 -0600

    correccion vecino mas proximo

--- tsp_vecino_proximo.py
+++ tsp_vecino_proximo.py
@@ -6,10 +6,10 @@ import sys
 
 def visitar_nodo(grafo,nodo):
     
-     
+    nodo_nuevo = 1 
     tupla = ()
     menor_peso = 11
-    pesos = nx.get_edge_attributes(grafo,'weight')
+   # pesos = nx.get_edge_attributes(grafo,'weight')
     #x es el vecino
     #y es la tupla de la arista
     
@@ -28,18 +28,32 @@ def visitar_nodo(grafo,nodo):
              tupla = y    
     """
     vecinos_visitados = len(visitados)
+    
     for x in grafo.neighbors(nodo):
-       for y in pesos:
-         if x in y: #el vecino esta en la arista
-           if x not in visitados: # el vecino no esta visitado
-              if vecinos_visitados < (len(grafo.nodes())-1): # mientas no tenga visitado al origen
-                 if nodo_inicio not in y:
-                    if pesos[y] < menor_peso: # buscando el menor peso
-                       menor_peso = pesos[y]
-                       nodo = x
-              else:
-                nodo = x      
-                     
+      print("nodo ",nodo," vecinos ",grafo.neighbors(nodo))
+      for y in pesos:
+        n1 = y[0]
+        n2 = y[1]
+      
+      
+         
+        if (x==n1 or x==n2) and (nodo==n1 or nodo==n2) and x not in visitados:
+             print(n1,",",n2," peso ",pesos[y])
+            
+             if vecinos_visitados < (len(grafo.nodes())-1):
+               if x == nodo_inicio:
+                 continue
+               
+               else:
+                 print ("el peso es :",pesos[y])
+                 if pesos[y] < menor_peso:
+                    print("el menor a cambiado")
+                    menor_peso = pesos[y]
+                    nodo_nuevo = x  
+       
+              
+             else:
+              nodo_nuevo = x          
                  
              
            
@@ -53,18 +67,19 @@ def visitar_nodo(grafo,nodo):
     
     
     #nodo = nodo_vecino(tupla,nodo)
+    print("---------------")
     print("el nodo visitado es")
-    print(nodo)
-    grafo.node[nodo]['visitado'] = 1
+    print(nodo_nuevo)
+    grafo.node[nodo_nuevo]['visitado'] = 1
     print("el nodo se marco como visitado")
-    print(grafo.node[nodo]['visitado'])
+    print(grafo.node[nodo_nuevo]['visitado'])
     nada = str(input())
-    visitados.append(nodo)
+    visitados.append(nodo_nuevo)
     print("nodos visitados")
     print(visitados)
     if todos_visitados(grafo) == False:
      print("hola")
-     visitar_nodo(grafo,nodo)
+     visitar_nodo(grafo,nodo_nuevo)
     else:
        visit = nx.get_node_attributes(grafo,'visitado')
        print(visit)
@@ -103,10 +118,10 @@ g = nx.Graph()
 g.add_nodes_from([1,2,3,4,5,6,7,8],visitado=0)
 
 
-g.add_weighted_edges_from([(1,2,3),(1,3,4),(1,5,8),(1,4,8),(2,4,4),(2,3,4),(3,5,6),(3,4,6),(3,7,5),(3,8,10),(4,8,3),(4,7,8),(5,6,4),(5,7,10),(6,7,6),(7,8,4)])
+g.add_weighted_edges_from([(1,2,3),(1,3,4),(1,5,8),(1,4,8),(2,4,4),(2,3,4),(3,5,6),(3,6,5),(3,4,6),(3,7,5),(3,8,10),(4,8,3),(4,7,8),(5,6,4),(5,7,10),(6,7,6),(7,8,4)])
 visitados = []
 
-#pesos = nx.get_edge_attributes(g,'weight')
+pesos = nx.get_edge_attributes(g,'weight')
 #for x in pesos:
 # print(x)
 # print(pesos[x])
@@ -122,4 +137,3 @@ print (lista)
 
 nx.draw_circular(g)
 plt.show()
-
